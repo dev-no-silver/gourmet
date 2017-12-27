377d9a5875481f093c8eb7e3adf3607e8b580d30
dev-no-silver <dev.yasunari.kondo@gmail.com>
2017-12-28 03:52:51 +0900

    位置情報から検索

--- GourmetSearch/ShopList/SearchTopTableViewController.swift
+++ GourmetSearch/ShopList/SearchTopTableViewController.swift
@@ -7,13 +7,85 @@
 //
 
 import UIKit
+import CoreLocation
 
 class SearchTopTableViewController: UITableViewController {
     
     var freeword: UITextField? = nil
 
+    let ls = LocationService()
+    let nc = NotificationCenter.default
+    var observers = [NSObjectProtocol]()
+    var here: (lat: Double, lon: Double)? = nil
+
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        // 位置情報取得を禁止している場合
+        observers.append(
+            nc.addObserver(forName: .authDenied, object: nil, queue: nil, using: {
+                notification in
+                // 位置情報がONになっていないダイアログ表示
+                self.present(self.ls.locationServiceDisabledAlert,
+                             animated: true,
+                             completion: nil)
+            })
+        )
+
+        // 位置情報取得を制限している場合
+        observers.append(
+            nc.addObserver(forName: .authRestricted, object: nil, queue: nil, using: {
+                notification in
+                // 位置情報が制限されているダイアログ表示
+                self.present(self.ls.locationServiceRestrictedAlert,
+                             animated: true,
+                             completion: nil)
+            })
+        )
+
+        // 位置情報取得に失敗した場合
+        observers.append(
+            nc.addObserver(forName: .didFailLocation, object: nil, queue: nil, using: {
+                notification in
+                // 位置情報取得に失敗したダイアログ
+                self.present(self.ls.locationServiceDidFailAlert,
+                             animated: true,
+                             completion: nil)
+            })
+        )
+
+        // 位置情報を取得した場合
+        observers.append(
+            nc.addObserver(forName: .didUpdateLocation, object: nil, queue: nil, using: {
+                notification in
+
+                // 位置情報が渡されていなければ早期離脱
+                guard let userInfo = notification.userInfo as? [String: CLLocation] else {
+                    return
+                }
+
+                // userInfoがキー location を持っていなければ早期離脱
+                guard let clloc = userInfo["location"] else {
+                    return
+                }
+
+                self.here = (
+                    lat: clloc.coordinate.latitude,
+                    lon: clloc.coordinate.longitude
+                )
+
+                self.performSegue(withIdentifier: "PushShopListFromHere", sender: self)
+            })
+        )
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        for observer in observers {
+            nc.removeObserver(observer)
+        }
+        observers = []
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,7 +96,12 @@ class SearchTopTableViewController: UITableViewController {
         freeword?.resignFirstResponder()
     }
 
+
     // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 44
+    }
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
@@ -33,24 +110,46 @@ class SearchTopTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         switch section {
         case 0:
-            return 1
+            return 2
         default:
             return 0
         }
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if indexPath.section == 0 && indexPath.row == 0 {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "Freeword") as! FreewordTableViewCell
-            self.freeword = cell.freeword
-            
-            cell.freeword.delegate = self
-            cell.selectionStyle = .none
-            
-            return cell
+        if indexPath.section == 0 {
+            switch indexPath.row {
+            case 0:
+                let cell = tableView.dequeueReusableCell(withIdentifier: "Freeword") as! FreewordTableViewCell
+                // UITextFieldへの参照を保存しておく
+                freeword = cell.freeword
+                // UITextFieldDelegateを自身に設定
+                cell.freeword.delegate = self
+                // タップを無視
+                cell.selectionStyle = .none
+
+                return cell
+
+            case 1:
+                let cell = UITableViewCell()
+                cell.textLabel?.text = "現在地から検索"
+                cell.accessoryType = .disclosureIndicator
+
+                return cell
+
+            default:
+                return UITableViewCell()
+            }
         }
 
         return UITableViewCell()
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.section == 0 && indexPath.row == 1 {
+            ls.startUpdatingLocation()
+            tableView.deselectRow(at: indexPath, animated: true)
+        }
     }
 }
 
@@ -59,7 +158,7 @@ extension SearchTopTableViewController: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         performSegue(withIdentifier: "PushShopList", sender: self)
-        
+
         return true
     }
 }
@@ -71,5 +170,20 @@ extension SearchTopTableViewController {
             let vc = segue.destination as! ShopListViewController
             vc.yls.condition.query = freeword?.text
         }
+
+        if segue.identifier == "PushShopListFromHere" {
+            let vc = segue.destination as! ShopListViewController
+            vc.yls.condition.lat = self.here?.lat
+            vc.yls.condition.lon = self.here?.lon
+        }
+    }
+}
+
+extension SearchTopTableViewController: UIGestureRecognizerDelegate {
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
+        if let ifr = freeword?.isFirstResponder {
+            return ifr
+        }
+        return false
     }
 }
