c74c55b0a73d6af555e7be248b588ec265611755
dev-no-silver <dev.yasunari.kondo@gmail.com>
2017-12-29 21:27:49 +0900

    photoListViewController

---
+++ GourmetSearch/PhotoList/PhotoListItemCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoListItemCollectionViewCell.swift
+//  GourmetSearch
+//
+//  Created by Yasunari Kondo on 2017/12/29.
+//  Copyright © 2017年 Yasunari Kondo. All rights reserved.
+//
+
+import UIKit
+
+class PhotoListItemCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var photo: UIImageView!
+}
---
+++ GourmetSearch/PhotoList/PhotoListItemCollectionViewHeader.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoListItemCollectionViewHeader.swift
+//  GourmetSearch
+//
+//  Created by Yasunari Kondo on 2017/12/29.
+//  Copyright © 2017年 Yasunari Kondo. All rights reserved.
+//
+
+import UIKit
+
+class PhotoListItemCollectionViewHeader: UICollectionReusableView {
+        
+    @IBOutlet weak var title: UILabel!
+}
