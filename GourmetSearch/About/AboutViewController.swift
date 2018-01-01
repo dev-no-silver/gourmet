5fbc57a100bca246cfe6e943efc1b44bf270914e
dev-no-silver <dev.yasunari.kondo@gmail.com>
2018-01-02 01:07:59 +0900

    fix select

--- GourmetSearch/About/AboutViewController.swift
+++ GourmetSearch/About/AboutViewController.swift
@@ -43,4 +43,8 @@ extension AboutViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 2
     }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
